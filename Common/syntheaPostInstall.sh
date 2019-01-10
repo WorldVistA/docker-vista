3faf0910259ea61591573d94b439a2449f79bcf2
Joseph Snyder <joe.snyder@kitware.com>
2019-01-10 11:25:54 -0500

    Fix up directory and path usage
    Ensure that only one pushd/popd is used to eliminate confusion.  Use the
    vistaDir to call the PatchSequenceApply file within the /tmp/loader
    directory.

--- Common/syntheaPostInstall.sh
+++ Common/syntheaPostInstall.sh
@@ -4,7 +4,6 @@ pushd /tmp/loader
 # Download Synthea loader multibuild
 curl -fsSL --progress-bar https://github.com/OSEHRA/VistA-FHIR-Data-Loader/releases/download/0.1/VISTA_SYNTHETIC_DATA_LOADER_BUNDLE_0P2T1.KID.zip -o loader.zip
 unzip -q loader.zip -d /tmp/loader/Synthea
-popd
 echo "Installing Synthea ingestor patch"
 # Set up arguments for PatchSequenceApply scrip based upon system
 system=1
@@ -17,18 +16,15 @@ fi
 vistaDir="$basedir/Dashboard/VistA/Scripts"
 # But check to be sure and download VistA if necessary
 if [ ! -d $vistaDir ]; then
-  pushd /tmp/loader
   echo "Downloading OSEHRA VistA Tester Repo"
   curl -fsSL --progress-bar https://github.com/OSEHRA/VistA/archive/master.zip -o VistA-master.zip
   unzip -q VistA-master.zip
   rm VistA-master.zip
   mv VistA-master VistA
   vistaDir="/tmp/loader/VistA/Scripts"
-  popd
 fi
 # Installs as the DUZ=1 user
-pushd $vistaDir
 # Installs the single KIDS build for Synthea loader as DUZ=1 user.
-python PatchSequenceApply.py -S $system -p /tmp/loader/Synthea $instanceName -l /tmp/loader -i -n ALL -d 1
+python $vistaDir/PatchSequenceApply.py -S $system -p /tmp/loader/Synthea $instanceName -l /tmp/loader -i -n ALL -d 1
 popd
-rm -rf /tmp/loader
+rm -r /tmp/loader
