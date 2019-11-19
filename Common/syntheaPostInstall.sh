01161ed90703ba93bd05f5b5bd2b98dd188e5a75
Sam Habiel <sam.habiel@gmail.com>
2019-11-19 15:20:35 -0500

    Updated for latest version + Python 3

--- Common/syntheaPostInstall.sh
+++ Common/syntheaPostInstall.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-# Get RGNETWWW and put it in the routine directory. Required by SYN_1_0_2.KID
-pushd $basedir/r/
-curl -LO https://raw.githubusercontent.com/mdgeek/VistA-Network-Services/master/src/RGNETWWW.m
-dos2unix RGNETWWW.m
-popd
-
 mkdir -p /tmp/loader/Synthea
 pushd /tmp/loader
+pip3 install --user chardet future pexpect
 
-# Download Synthea loader multibuild
-curl -fsSL --progress-bar https://github.com/OSEHRA/VistA-FHIR-Data-Loader/releases/download/0.3/VISTA_FHIR_DATA_LOADER_BUNDLE_0P3.KID.zip -o loader.zip
+# Download loader multibuild
+curl -fsSL --progress-bar https://github.com/OSEHRA/VistA-FHIR-Data-Loader/releases/download/0.4/VISTA_FHIR_DATA_LOADER_BUNDLE_0P4.KID.zip -o loader.zip
 unzip -q loader.zip -d /tmp/loader/Synthea
-
-# Download FHIR Display Code
-pushd ./Synthea
-curl -fsSLO --progress-bar https://raw.githubusercontent.com/OSEHRA/FHIR-on-VistA/master/VistA-REST-services/kids/SYN_1_0_2.KID
-dos2unix *.KID
-popd
-
+recode -f iso8859-1..ascii /tmp/loader/Synthea/*
 
 echo "Installing Synthea ingestor patch"
 # Set up arguments for PatchSequenceApply script based upon system
@@ -40,6 +29,5 @@ if [ ! -d $vistaDir ]; then
 fi
 # Installs as the DUZ=1 user
 # Installs the single KIDS build for Synthea loader as DUZ=1 user.
-python $vistaDir/PatchSequenceApply.py -S $system -p /tmp/loader/Synthea $instanceName -l /tmp/loader -i -n ALL -d 1
+python3 $vistaDir/PatchSequenceApply.py -S $system -p /tmp/loader/Synthea $instanceName -l /tmp/loader -i -n ALL -d 1
 popd
-rm -r /tmp/loader
