891800ea4e9925eecb6ea0b1dc0c3ce3f4f11fb5
Sam Habiel <sam@yottadb.com>
2023-06-08 15:57:52 -0400

    Add instance name

--- GTM/bin/repl/recv/repl_setup.sh
+++ GTM/bin/repl/recv/repl_setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 $gtm_dist/mupip set -replication=on -region DEFAULT
-$gtm_dist/mupip replicate -instance_create -noreplace
+$gtm_dist/mupip replicate -instance_create -noreplace -name=secondary
