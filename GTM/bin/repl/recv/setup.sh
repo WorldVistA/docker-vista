e36c2f01d501714f2683bb372edc239ef76281a9
Sam Habiel <sam@yottadb.com>
2023-10-30 11:02:44 -0400

    Bug fix for replication receiver set-up

--- GTM/bin/repl/recv/setup.sh
+++ GTM/bin/repl/recv/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 instname=$1
 $gtm_dist/mupip set -replication=on -region DEFAULT
-$gtm_dist/mupip replicate -instance_create -name=$instname
+$gtm_dist/mupip replicate -editinstance -name=$instname $gtm_repl_instance
