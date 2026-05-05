902fc37976f71a5340829a2ccf8a04dd5c16ca05
Sam Habiel <sam@smh101.com>
2026-05-05 19:41:16 -0400

    [IRIS] Install VPE on FOIA image on IRIS

---
+++ IRIS/docker-iris/install_vpe.sh
@@ -0,0 +1,28 @@
+#---------------------------------------------------------------------------
+# Copyright 2026 Sam Habiel
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+#
+#!/bin/bash
+set -xve
+iris session IRIS -U VISTA %RI<<END
+/tmp/VPE15P2.RSA
+
+Y
+0
+A
+Y
+Y
+Y
+END
